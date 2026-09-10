228f6014d1eba3882e20f6ffb78ffd5355f08d10
hasini-tatikonda <hasinitatikonda17@gmail.com>
2026-09-10 22:28:14 +0530

    Time: 13 ms (9.55%), Space: 47.4 MB (9.84%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
+        String[] rows=new String[numRows];
+        for(int i=0;i<numRows;i++){
+            rows[i]="";
+
+        }
+        int row=0;
+        boolean down=true;
+        for(int i=0;i<s.length();i++){
+            rows[row]+=s.charAt(i);
+            if(row==numRows-1){
+                down=false;
+            }
+            if(row==0){
+                down=true;
+            }
+            if(down){
+                row++;
+            }
+            else{
+                row--;
+            }
+        }
+        String answer="";
+        for(int i=0;i<numRows;i++){
+            answer+=rows[i];
+        }
+        return answer;
+    }
+}
