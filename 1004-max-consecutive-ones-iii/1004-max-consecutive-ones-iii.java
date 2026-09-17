8250a46ee3cf14c75c8ade8ce0b72e2855cac8f2
hasini-tatikonda <hasinitatikonda17@gmail.com>
2026-09-17 18:55:00 +0530

    Time: 4 ms (44.42%), Space: 52.4 MB (6.47%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,28 +1,18 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int l=0;
-       // int r=0;
         int max=0;
         for(int r=0;r<nums.length;r++){
-            if(nums[r]==1){
-               // r++;
-                max=Math.max(max,r-l+1);
-            }
-            else if(nums[r]==0&&k!=0){
-                //r++;
+            if(nums[r]==0&&k!=0){
                 k--;
-                max=Math.max(max,r-l+1);
             }
-            else if(k==0){
-            while(nums[l]!=0){
-                
-                        l++;
-            }
-                        //k++;
-           
+            else if(nums[r]==0&&k==0){
+                while(nums[l]!=0){
+                    l++;
+                }
                 l++;
-                max=Math.max(max,r-l+1);
             }
+            max=Math.max(max,r-l+1);
         }
         return max;
     }
